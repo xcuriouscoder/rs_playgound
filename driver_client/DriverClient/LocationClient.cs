82774c44efdbbf4994e7da67cf565e54c0094071
xcuriouscoder <derrickryalls@gmail.com>
2025-10-20 17:37:07 -0700

    Add a caller to the location service

---
+++ driver_client/DriverClient/LocationClient.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+namespace DriverClient
+{
+    public class LocationClient
+    {
+        HttpClient client = new HttpClient();
+        Random random = new Random();
+        Timer timer;
+
+        public string DriverId { get; } = Guid.NewGuid().ToString();
+
+        public int Count { get; private set; } = 0;
+
+        public LocationClient()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Add("userid", DriverId);
+            client.BaseAddress = new Uri("http://localhost:3002");
+
+            timer = new Timer(async _ => 
+            {
+                try
+                {
+                    await ProcessRepositoriesAsync();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error processing location for driver {DriverId} Count {Count}: {ex.Message}");
+                }
+            }, 
+            null, 
+            Timeout.Infinite, 
+            Timeout.Infinite);
+        }
+
+        internal void StartLocationCalls()
+        {
+            Count = 0;
+            timer.Change(TimeSpan.FromSeconds(0), TimeSpan.FromSeconds(5));
+        }
+
+        internal void StopLocationCalls()
+        {
+            timer.Change(Timeout.Infinite, Timeout.Infinite);
+        }
+
+        internal async Task ProcessRepositoriesAsync()
+        {
+
+            var data = JObject.FromObject(new
+            {
+                latitude = 47 + random.NextDouble(),
+                longitude = -122 + random.NextDouble()
+            });
+
+            var response = await client.PostAsync("/locations",
+                    new StringContent(JsonConvert.SerializeObject(data),
+                    Encoding.UTF8, 
+                    "application/json"));
+
+            response.EnsureSuccessStatusCode();
+
+            this.Count++;
+
+            var responseContent = await response.Content.ReadAsStringAsync();
+        }
+    }
+}
---
+++ driver_client/DriverClient/Program.cs
@@ -0,0 +1,49 @@
+﻿using DriverClient;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        var locClient = new LocationClient();
+
+        var driverCount = 1000;
+
+        var drivers = new List<LocationClient>(driverCount+1);
+
+        Console.WriteLine($"Creating {driverCount} drivers.");
+
+        for (int i = 0; i < driverCount; i++)
+        {
+            var client = new LocationClient();
+            drivers.Add(client);
+        }
+
+        Console.WriteLine("Starting location updates.");
+
+        foreach (var driver in drivers)
+        {
+            driver.StartLocationCalls();
+            await Task.Delay(1);
+        }
+
+        Console.WriteLine("Drivers started.  Waiting...");
+
+        await Task.Delay(60000);
+
+        foreach (var driver in drivers)
+        {
+            driver.StopLocationCalls();
+        }
+
+        Console.WriteLine("Drivers Stopped.");
+
+        foreach(var driver in drivers)
+        {
+            Console.WriteLine($"Driver {driver.DriverId} sent {driver.Count}");
+        }
+
+        var totalCount = drivers.Sum(d => d.Count);
+        Console.WriteLine($"Total location updates sent: {totalCount}");
+    }
+}
